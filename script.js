a37aa1acc7a358c639bdb38cf118247bbfb9fc27
Sam McCormick <samjmccormick@gmail.com>
2024-09-15 16:04:58 -0700

    created recursive function to return change, based on roman numeral converted need to combine that with a check on the amount of cash in the drawer then update the cash in draw amounts

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const changeDueDiv = document.getElementById("change-due");
+const userInputCash = document.getElementById("cash");
+const purchaseBtn = document.getElementById("purchase-btn");
+const priceDiv = document.getElementById("price");
+const cidDIV = document.getElementById("cid");
+
+const moneyValue = [
+{name: 'ONE HUNDRED',
+value: 100.00},
+{name: 'TWENTY',
+value: 20.00},
+{name: 'TEN',
+value: 10.00},
+{name: 'FIVE',
+value: 5.00},
+{name: 'ONE',
+value: 1.00},
+{name: 'QUARTER',
+value: 0.25},
+{name: 'DIME',
+value: 0.10},
+{name: 'NICKEL',
+value: 0.05},
+{name: 'PENNY',
+value: 0.01}
+];
+
+
+/* price and cash in draw variables */
+
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
@@ -10,3 +40,28 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+
+function changeDue (cash) {
+	if (cash === 0) {
+		return "";
+	}
+	
+	for (let i = 0; i < moneyValue.length; i++) {
+		if (cash.toFixed(2) >= moneyValue[i].value) {
+			console.log(cash.toFixed(2));
+			console.log(moneyValue[i].value);
+			return moneyValue[i].name + changeDue(cash.toFixed(2) - moneyValue[i].value)
+		}
+	}
+};
+
+function totalChange (price, customerCash) {
+	return customerCash.toFixed(2) - price.toFixed(2);
+};
+
+function checkCidAmount (demonination, cidAmount) {
+	
+	
+};
+
+console.log(changeDue(14.34));
