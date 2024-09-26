10bfd433830a1344b864860cd3365a3f1650cd4f
samjmccormick <samjmccormick@gmail.com>
2024-09-26 11:51:42 -0700

    move around

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let price = 19.5;
+let cid = [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
+
 const changeDueDiv = document.getElementById("change-due");
 const userInputCash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
@@ -50,8 +53,7 @@ const moneyValue = [
 
 /* price and cash in draw variables */
 
-let price = 19.5;
-let cid = [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
+
 
 let accumulator = 0;
 
